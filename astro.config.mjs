4f9dbf4c9f5a6d06ff29faf53a12993f3f29ea28
landfallbox <landfallbox@gmail.com>
2026-05-25 22:43:28 +0800

    configure github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
 
+const site = process.env.SITE_URL ?? 'https://landfallbox.github.io';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site,
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
