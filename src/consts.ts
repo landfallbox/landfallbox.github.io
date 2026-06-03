6d183461fbba60c0b72a09f878b08a8c4d23bd67
landfallbox <landfallbox@gmail.com>
2026-06-03 23:48:08 +0800

    chore(site): 更新站点标题文案

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Landfallbox 博客';
+export const SITE_TITLE = 'Landfallbox 的博客';
 export const SITE_DESCRIPTION = '关于代码、工具和创作过程的笔记。';
